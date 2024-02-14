f5ad6a586fcce78bfa74cb176e5c0fe049dc8c4c
Ivan CM <56263882+Ivan21A7x@users.noreply.github.com>
2024-02-14 07:44:23 -0600

    Parcial01 - Added features in description
    Commit_inicial: Configuración del proyecto desde el último commit del semestre pasado.
    Commit#1_14-02-2024 Added and updated member_edit component.
    Commit#2_15-02-2024: Added NgForm and @Viewchild, added  can deactivate guard, updated the user in SPA app.
    Commit#3_19-02-2024: Added loading indicator/service, storing the state of the app via service.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+using System.Security.Claims;
+using API.DTOs;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+    public UsersController(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
+        var user = await _userRepository.GetMembersAsync();
+        return Ok(user);
+    }
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
+        return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDto, user);
+
+        _userRepository.Update(user);
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("No se pudo realizar la operación");
+    }
+
+}
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using API.Helpers;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddAplicacionServices(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UsersRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
+        return services;
+    }
+}
